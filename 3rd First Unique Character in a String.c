69cb331442f4b09d33f597944042f80d29f00aac
athiradhkrishnavjecprint-jpg <athiradhkrishnavjecprint@gmail.com>
2026-01-31 15:35:57 +0530

    Add files via upload

---
+++ 3rd First Unique Character in a String.c
@@ -0,0 +1,18 @@
+#include <string.h>
+
+int firstUniqChar(char* s) {
+    int count[26] = {0};
+    int n = strlen(s);
+
+    for (int i = 0; i < n; i++) {
+        count[s[i] - 'a']++;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (count[s[i] - 'a'] == 1) {
+            return i;
+        }
+    }
+
+    return -1;
+}
